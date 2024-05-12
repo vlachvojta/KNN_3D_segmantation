e51c76fa65597d66212729f9d67fe1362bcee858
Preclaa <kneslik.m@gmail.com>
2024-05-12 13:24:36 +0200

    Update DataLoader for experiments

--- src/data_loader.py
+++ src/data_loader.py
@@ -13,7 +13,7 @@ random.seed(time.time())
 
 class DataLoader:
     def __init__(self, data_path, click_area=0.05, downsample=0, force=False, 
-                 verbose=True, normalize_colors=False, limit_to_one_object=False, voxel_size=0):
+                 verbose=True, normalize_colors=False, limit_to_one_object=False, voxel_size=0, n_of_clicks=0):
         self.data_path = data_path
         self.click_area = click_area
         self.downsample = downsample
@@ -21,11 +21,14 @@ class DataLoader:
         self.verbose = verbose
         self.normalize_colors = normalize_colors
         self.voxel_size = voxel_size
+        self.n_of_clicks = n_of_clicks
 
         assert os.path.exists(data_path), "Data path does not exist. Choose a valid path to a dataset."
+        
+        self.selected_object = None
 
         # self.cache_path = os.path.join(data_path, "dataloader_cache")
-        self.cache_path = os.path.join(data_path, "dataloader_cache_ds" + str(downsample) + ".pkl")
+        self.cache_path = os.path.join(data_path, "dataloader_cache_ds" + str(downsample) + "_nc" + str(n_of_clicks) + ".pkl")
 
         # Load from cache
         if os.path.exists(self.cache_path):
@@ -38,7 +41,7 @@ class DataLoader:
 
         self.data = {}
 
-        print(f'\nCreating DataLoader with click_area={click_area} and downsample={downsample}, processing {len([f for f in os.scandir(data_path)])} files.')
+        print(f'\nCreating DataLoader with click_area={click_area} and downsample={downsample} and n of clicks={n_of_clicks}, processing {len([f for f in os.scandir(data_path)])} files.')
         # Process each area
         for i, file in enumerate([f.path for f in os.scandir(data_path) if f.path.endswith('.pcd')]):
             if verbose:
@@ -65,32 +68,18 @@ class DataLoader:
             for group in groups:     
                 # Select every k point from each object
                 # First and last point are skipped
-                # 9 points total
-                points = [group[(i * (len(group) // 11))] for i in range(1, 10)]
-                random.shuffle(points)
                 
-                # groups of 1,1,2,2,3 clicks
-                pointsP = [points[0:1], points[1:2], points[2:4], points[4:6], points[6:9]]
-                random.shuffle(points)
-                   
-                # Negative click simulation
-                # pcd_copy = o3d.geometry.PointCloud()
-                # pts = pcd.point.positions.numpy()
-                # # Select labeled object and calculate center
-                # pcd_copy.points = o3d.utility.Vector3dVector(np.array(pts)[group])
-                # center = pcd_copy.get_center()
-                # # Select everything but the labeled object, get 4 points close to selected object
-                # pcd_copy.points = o3d.utility.Vector3dVector(np.array([pts[i] for i in range(len(pts)) if i not in group]))
-                # pcd_tree = o3d.geometry.KDTreeFlann(pcd_copy)
-                # [_, idx, _] = pcd_tree.search_knn_vector_3d(center, 4000)   
-                # random_indices = random.sample(sorted(idx[2000:]), 4)
-                # random.shuffle(random_indices)
+                if self.n_of_clicks == 0:
+                    points = [group[(i * (len(group) // 11))] for i in range(1, 10)]
+                    random.shuffle(points)
+                    # groups of 1,1,2,2,3 clicks
+                    points = [points[0:1], points[1:2], points[2:4], points[4:6], points[6:9]]
+                    random.shuffle(points)
+                else:
+                    points = [[group[(i * (len(group) // (self.n_of_clicks + 2)))]] for i in range(1, self.n_of_clicks + 1)]
+                    random.shuffle(points)
+                self.data[file].append(points)
                 
-                # # groups of 0,1,0,1,2
-                # pointsN = [[], random_indices[0:1], [], random_indices[1:2], random_indices[2:4]]
-                pointsN = [[],[],[],[],[]] 
-
-                self.data[file].append(list(zip(pointsP, pointsN)))
             random.shuffle(self.data[file])
             
         print('')
@@ -103,8 +92,51 @@ class DataLoader:
 
     def __getitem__(self, index):
         return self.get_random_batch()
+    
+    def process_click(self, points, area):
+        # Load pointcloud
+        pcd = o3d.t.io.read_point_cloud(area)
+        if self.downsample != 0:
+            pcd = pcd.uniform_down_sample(every_k_points=self.downsample)
+        
+    
+        # Create a copy of the pointcloud (KDTreeFlann doesn't support o3d.t.geometry.PointCloud or idk)
+        # It's ugly but it works
+        pcd_tree = o3d.geometry.PointCloud()
+        pcd_tree.points = o3d.utility.Vector3dVector(pcd.point.positions.numpy())
+        tree = o3d.geometry.KDTreeFlann(pcd_tree)
+    
+        # Create a click area for each simulated point
+        for point in points:
+            [_, idx, _] = tree.search_radius_vector_3d(pcd_tree.points[point], self.click_area)
+            for i in idx:
+                pcd.point.maskPositive[i] = 1
+
+
+        # Get group id for label
+        group = pcd.point.group[points].numpy()[0]
+
+        # Create a mask with the same group as the clicked point
+        label = (pcd.point.group.numpy() == group)
+        label = o3d.core.Tensor(label, o3d.core.uint8, o3d.core.Device("CPU:0")).numpy() #(dtype=np.int8)
+
+        # Add tuple of pointcloud and label to batch
+        coords = pcd.point.positions.numpy()
+        if self.voxel_size > 0:
+            coords = coords / self.voxel_size
+
+        feats = np.concatenate((pcd.point.colors.numpy(), pcd.point.maskPositive.numpy(), pcd.point.maskNegative.numpy()), axis=1, dtype=np.float32)
+        if self.normalize_colors:
+            feats[:, :3] = feats[:, :3] / 255
+
+        if self.verbose:
+            print(f"Simulated click - {area.split('/')[-1]}/object {group}/point {points}")
+
+        # Return the concatenated arrays
+        return coords, feats, label
 
     def get_random_batch(self):
+        # return random area/object every function call
         if not self.data:
             # Every point has been processed
             print("DataLoader: All points have been processed. Returning None.")
@@ -124,49 +156,40 @@ class DataLoader:
         else:                
             random.shuffle(self.data[random_area])
             
-        # Load pointcloud
-        pcd = o3d.t.io.read_point_cloud(random_area)
-        if self.downsample != 0:
-            pcd = pcd.uniform_down_sample(every_k_points=self.downsample)
+        return self.process_click(random_points, random_area)
+    
+    def get_batch_with_clicks(self, n_of_clicks):
+        # return same area/object every function call
+        if self.selected_object == None:
+            self.next_random_batch()
+        if self.selected_object == None:
+            # Every point has been processed
+            print("DataLoader: All points have been processed. Returning None.")
+            return None
         
+        if n_of_clicks > self.n_of_clicks:
+            n_of_clicks = self.n_of_clicks
+            
+        points = [point for points in self.selected_object[:n_of_clicks] for point in points]
+        return self.process_click(points, self.selected_area)
     
-        # Create a copy of the pointcloud (KDTreeFlann doesn't support o3d.t.geometry.PointCloud or idk)
-        # It's ugly but it works
-        pcd_tree = o3d.geometry.PointCloud()
-        pcd_tree.points = o3d.utility.Vector3dVector(pcd.point.positions.numpy())
-        tree = o3d.geometry.KDTreeFlann(pcd_tree)
-    
-        # Create a click area for each simulated point
-        for point in random_points[0]:
-            [_, idx, _] = tree.search_radius_vector_3d(pcd_tree.points[point], self.click_area)
-            for i in idx:
-                pcd.point.maskPositive[i] = 1
-        for point in random_points[1]:
-            [_, idx, _] = tree.search_radius_vector_3d(pcd_tree.points[point], self.click_area)
-            for i in idx:
-                pcd.point.maskNegative[i] = 1
+    def next_random_batch(self):
+        if not self.data:
+            self.selected_object = None
+            return
+        
+        # Select random area, object and point
+        random_area = random.choice(list(self.data.keys()))
+        random_object = random.randint(0, len(self.data[random_area])-1)
 
-        # Get group id for label
-        group = pcd.point.group[random_points[0]].numpy()[0]
+        self.selected_object = self.data[random_area][random_object]
+        self.selected_area = random_area
 
-        # Create a mask with the same group as the clicked point
-        label = (pcd.point.group.numpy() == group)
-        label = o3d.core.Tensor(label, o3d.core.uint8, o3d.core.Device("CPU:0")).numpy() #(dtype=np.int8)
-
-        # Add tuple of pointcloud and label to batch
-        coords = pcd.point.positions.numpy()
-        if self.voxel_size > 0:
-            coords = coords / self.voxel_size
-
-        feats = np.concatenate((pcd.point.colors.numpy(), pcd.point.maskPositive.numpy(), pcd.point.maskNegative.numpy()), axis=1, dtype=np.float32)
-        if self.normalize_colors:
-            feats[:, :3] = feats[:, :3] / 255
-
-        if self.verbose:
-            print(f"Simulated click - {random_area.split('/')[-1]}/object {group}/point {random_points}")
-
-        # Return the concatenated arrays
-        return coords, feats, label
+        del self.data[random_area][random_object]
+        if not self.data[random_area]:
+            del self.data[random_area]
+        else:                
+            random.shuffle(self.data[random_area])
 
     def list_to_batch(self, clouds, dtype):
         max_len = max([len(cloud) for cloud in clouds])
