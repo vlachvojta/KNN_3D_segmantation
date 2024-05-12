68ee9a9bae78f2ce151b1fc5722df725b7e38e84
vlachvojta <vojtik.vlach@seznam.cz>
2024-05-12 15:09:13 +0200

    Add voxel_click IOU metric + results (chart is now group bat plot)

---
+++ meassurements/voxel_click_IOU.md
@@ -0,0 +1,10 @@
+# Voxel click IOU
+
+Effect of voxel size and click area on mean IOU in S3DIS dataset.
+
+Meassured so far on `dataset/S3DIS_converted_downsampled_new_mini/test` having 10 point cloud files. 
+With testing 5 click combinations for every object in scene it had 1300 unique input scenarios.
+
+Meassured by `find_best_voxel_click.py` and visualized by `visualize_best_voxel_click.py`.
+
+Best voxel click: Voxel_size = 0.07, Click area = 0.07.
--- src/visualize_best_voxel_clicks.py
+++ src/visualize_best_voxel_clicks.py
@@ -4,37 +4,91 @@ from matplotlib import pyplot as plt
 import csv
 
 # Load the results from the file
-# DIR='../results/testing_voxel_click_mini_mini'
 DIR='../results/testing_voxel_click_mini'
 RESULT_FILE = os.path.join(DIR, 'result_summary.txt')
 print(f'Loading results from {RESULT_FILE}')
 
-# read RESULT_FILE as is and print
-# with open(RESULT_FILE) as f:
-#     print(f.read())
-
 # read RESULT_FILE as csv and print. ignore headers!!
 with open(RESULT_FILE, 'r') as f:
     reader = csv.reader(f, delimiter=',', skipinitialspace=True)
-    rows = [row for row in reader if row[2] != 'None'][1:]
+    rows = [row for row in reader if row[2] != 'None']
+    if rows[0][0] == 'voxel_size':
+        rows = rows[1:]
+
+    rows = [[float(val) for val in row] for row in rows]
+    rows = [[voxel_size, click_area, round(IOU, 2)] for voxel_size, click_area, IOU in rows if IOU != 0]
 
 for row in rows:
     print(row)
 
-results = np.array(rows, dtype=float)
-voxel_sizes = results[:,0]
-click_areas = results[:,1]
-ious = results[:,2]
+data_orig = rows
 
-# Plot the results as a heatmap (x = click_area, y = voxel_size, color = iou)
-plt.figure()
-plt.scatter(click_areas, voxel_sizes, c=ious, cmap='viridis')
-plt.xlabel('Click Area')
-plt.ylabel('Voxel Size')
-plt.title('IoU')
-plt.colorbar()
+def get_click_voxel_ratio(click_area, voxel_size):
+    return round(click_area / voxel_size, 1)
+
+# data with voxel_click ratio:
+data = [(get_click_voxel_ratio(click_area, voxel_size), voxel_size, IOU) for voxel_size, click_area, IOU in data_orig]
+
+# Arrange data by voxel_click_ratio and voxel_size
+data_dict = {}
+for voxel_click_ratio, voxel_size, IOU in data:
+    if voxel_click_ratio not in [1, 1.5, 2]:
+        continue
+    if voxel_click_ratio not in data_dict:
+        data_dict[voxel_click_ratio] = {}
+    data_dict[voxel_click_ratio][voxel_size] = IOU
+
+# Sort the data by voxel_size
+data_dict_sorted = {}
+for voxel_click_ratio, results in data_dict.items():
+    # results = sorted(results, key=lambda x: x[0])
+    results = [results[voxel_size] for voxel_size in sorted(results.keys())]
+    # results = [round(IOU, 2) for voxel_size, IOU in results]
+    data_dict_sorted[f'{voxel_click_ratio}*voxel_size'] = results
+
+print('data dict sorted:')
+for k, v in data_dict_sorted.items():
+    print(k, v)
+
+ratios = list(map(str, np.unique([voxel_size for voxel_size, _, _ in data_orig])))
+ratio_restults = data_dict_sorted
+
+"""
+Data should look something like this:
+ratios = ("0.01", "0.02", "0.03", "0.04")
+ratio_restults = {
+    '1x': (18.35, 18.43, 14.98, 17.75),
+    '1.5x': (38.79, 48.83, 47.50, 22.22),
+    '2x': (189.95, 195.82, 217.19, 34.88),
+}
+"""
+
+# Prepare plot
+x = np.arange(len(ratios))  # the label locations
+width = 0.25  # the width of the bars
+multiplier = 0
+
+fig, ax = plt.subplots(layout='constrained')
+
+for attribute, measurement in ratio_restults.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, measurement, width, label=attribute)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Mean IOU')
+ax.set_title('Effect of voxel size and click area on mean IOU in S3DIS dataset.')
+ax.set_xticks(x + width, ratios)
+ax.set_xlabel('Voxel size')
+ax.legend(loc='upper left', ncols=3)
 plt.tight_layout()
-out_file = os.path.join(DIR, 'heatmap.png')
-plt.savefig(out_file)
+ax.set_ylim(0, max([max(measurement) for measurement in ratio_restults.values()]) + 10)
+
+# Save the plot
+out_file_png = os.path.join(DIR, 'voxel_click_IOU.png')
+out_file_svg = os.path.join(DIR, 'voxel_click_IOU.svg')
+plt.savefig(out_file_png)
+plt.savefig(out_file_svg)
 plt.clf()
-print(f'Heatmap saved to {out_file}')
+print(f'Groupbar chart saved to {out_file_png} and {out_file_svg}')
